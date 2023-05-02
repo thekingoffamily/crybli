5c25097ed73dd336f09e90f22b4b1bba7515fbaf
Виталий С <37731091+thekingoffamily@users.noreply.github.com>
2023-05-02 18:02:52 +0300

    Update App.vue

--- crybli/src/App.vue
+++ crybli/src/App.vue
@@ -1,7 +1,38 @@
 <template>
-  <p>Crybli will be work very soon =)</p>
+  <div>
+    <header>
+      <img src="../public/img/icons/crybli.jpg" class="logo" alt="logo" />
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <div class="white-line"></div>
+        <a href="https://github.com/thekingoffamily/crybli/tree/master/crybli" target="_blank">Crybli's startup teams github repository</a>
+    </footer>
+  </div>
 </template>
 
-<style scoped>
+<style>
+  /* Стили для белой полосы в футере */
+  .white-line {
+    height: 3px;
+    background-color: #fff;
+  }
 
+  /* Стили для ссылки на GitHub репозиторий */
+  footer a {
+    display: block;
+    text-align: center;
+    margin-top: 10px;
+    font-size: 18px;
+  }
+
+  /* Стили для логотипа в хедере */
+  .logo {
+    display: block;
+    margin: 20px auto;
+    width: 150px;
+    height: 150px;
+  }
 </style>
